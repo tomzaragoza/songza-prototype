d90b8d686ed420c4e288ef60c5a56d7d170f0de3
Tom Zaragoza <tomdzaragoza@gmail.com>
2014-05-13 20:46:13 -0400

    Display the similar curators to Jesse Sussman. work on making it work for all curator pages

--- app.py
+++ app.py
@@ -11,13 +11,22 @@ collection = db['curators']
 @app.route("/")
 def main():
 	filename = "Playlists on Songza.html"
-	# curator = collection.find_one({'name': 'Jesse Sussman'})
-	# image_url = curator['image_url']
+	
+	# in this example, use Jesse Sussman
+	curator = collection.find_one({'name': 'Jesse Sussman'})
 
-	# once recommendations have been made, load them here to 
-	# pass into the page
 
-	return render_template(filename, curators=[curator])
+	similar_curators = curator['similar_curators']
+	similar_curators.sort()
+	similar_curators.reverse()
+
+	final_similar_curators = []
+	for curator in similar_curators:
+		d = collection.find_one({'name': curator[1]})
+		final_similar_curators.append(collection.find_one({'name': curator[1]}))
+
+
+	return render_template(filename, similar_curators=final_similar_curators)
 
 if __name__ == "__main__":
     app.run()
