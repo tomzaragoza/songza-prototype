854b2585b51fcee512664d34b437e93411654b81
Tom Zaragoza <tomdzaragoza@gmail.com>
2014-05-13 20:47:12 -0400

    saving progress on my attemp to use PredictionIO

--- show.py
+++ show.py
@@ -1,4 +1,4 @@
-import predictionio
+import predictionio	
 from pymongo import MongoClient
 
 import sys
@@ -15,14 +15,18 @@ curator_names = []
 
 for curator in collection.find():
 	# User Data
-	curator_names.append(curator['name'])
+	curator_names.append({'name': curator['name'], 'featured_artists':curator['featured_artists']})
 
 # Recommend 5 curators to each user
 for curator in curator_names:
-    print "Retrieve top 10 recommendations for Curator", curator
-    try:
-        client.identify(curator)
-        rec = client.get_itemrec_topn("engine1", 10)
-        print rec
-    except predictionio.ItemRecNotFoundError as e:
-        print 'Caught exception:', e.strerror()
+	print "====================================================="
+	print "Retrieve for", curator
+	try:
+		client.identify(curator['name'])
+		for artist in curator['featured_artists']:
+			rec = client.get_itemsim_topn("engine2", artist, 10)
+			print rec
+		# rec = client.get_itemrec_topn("engine1", 5)
+		# print rec
+	except predictionio.ItemRecNotFoundError as e:
+		print 'Caught exception:', e.strerror()
